7777a0fc1ebc48a7a594fbfb81815a978f9258cf
Rushab Shah <rushabshah32@gmail.com>
2020-08-02 03:51:41 +0530

    GestureAnywhere: Add Gesture Anywhere validators
    * Import Gravity class for the GA Location
    * Create a new Long type Validator class
    
    Change-Id: I9021bcf44ccadec114b336978d07ef281c1e6f31

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -33,6 +33,7 @@ import android.util.AndroidException;
 import android.util.ArrayMap;
 import android.util.ArraySet;
 import android.util.Log;
+import android.view.Gravity;
 
 import com.android.internal.util.ArrayUtils;
 
@@ -416,6 +417,25 @@ public final class LineageSettings {
         }
     }
 
+    private static final class InclusiveLongRangeValidator implements Validator {
+        private final long mMin;
+        private final long mMax;
+
+        public InclusiveLongRangeValidator(long min, long max) {
+            mMin = min;
+            mMax = max;
+        }
+
+        @Override
+        public boolean validate(String value) {
+            try {
+                final long longValue = Long.parseLong(value);
+                return longValue >= mMin && longValue <= mMax;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+    }
     private static final class InclusiveFloatRangeValidator implements Validator {
         private final float mMin;
         private final float mMax;
@@ -1720,15 +1740,24 @@ public final class LineageSettings {
 
         /**
          * Whether to use gesture anywhere feature.
+         * 0 = 0ff, 1 = on
          * @hide
          */
         public static final String GESTURE_ANYWHERE_ENABLED = "gesture_anywhere_enabled";
+
+        /** @hide */
+        public static final Validator GESTURE_ANYWHERE_ENABLED_VALIDATOR =
+                sBooleanValidator;
 
         /**
          * Position of gesture anywhere trigger.  Value is either Gravity.LEFT or Gravity.RIGHT
          * @hide
          */
         public static final String GESTURE_ANYWHERE_POSITION = "gesture_anywhere_position";
+
+        /** @hide */
+        public static final Validator GESTURE_ANYWHERE_POSITION_VALIDATOR =
+                new InclusiveIntegerRangeValidator(Gravity.LEFT, Gravity.RIGHT);
 
         /**
          * Last time gestures were altered.
@@ -1737,11 +1766,19 @@ public final class LineageSettings {
          */
         public static final String GESTURE_ANYWHERE_CHANGED = "gesture_anywhere_changed";
 
+        /** @hide */
+        public static final Validator GESTURE_ANYWHERE_CHANGED_VALIDATOR =
+                new InclusiveLongRangeValidator(Long.MIN_VALUE, Long.MAX_VALUE);
+
         /**
          * Width of the gesture anywhere trigger.
          * @hide
          */
         public static final String GESTURE_ANYWHERE_TRIGGER_WIDTH = "gesture_anywhere_trigger_width";
+
+        /** @hide */
+        public static final Validator GESTURE_ANYWHERE_TRIGGER_WIDTH_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 64);
 
         /**
          * Position of gesture anywhere trigger.
@@ -1749,11 +1786,19 @@ public final class LineageSettings {
          */
         public static final String GESTURE_ANYWHERE_TRIGGER_TOP = "gesture_anywhere_trigger_top";
 
+        /** @hide */
+        public static final Validator GESTURE_ANYWHERE_TRIGGER_TOP_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 99);
+
         /**
          * Height of the gesture anywhere trigger.
          * @hide
          */
         public static final String GESTURE_ANYWHERE_TRIGGER_HEIGHT = "gesture_anywhere_trigger_height";
+
+        /** @hide */
+        public static final Validator GESTURE_ANYWHERE_TRIGGER_HEIGHT_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 100);
 
         /**
          * Whether to display the gesture anywhere trigger region or not.
@@ -1761,6 +1806,10 @@ public final class LineageSettings {
          * @hide
          */
         public static final String GESTURE_ANYWHERE_SHOW_TRIGGER = "gesture_anywhere_show_trigger";
+
+        /** @hide */
+        public static final Validator GESTURE_ANYWHERE_SHOW_TRIGGER_VALIDATOR =
+                sBooleanValidator;
 
         /**
          * Whether to show the alarm clock icon in the status bar.
@@ -2449,6 +2498,17 @@ public final class LineageSettings {
                     CLICK_PARTIAL_SCREENSHOT_VALIDATOR);
             VALIDATORS.put(__MAGICAL_TEST_PASSING_ENABLER,
                     __MAGICAL_TEST_PASSING_ENABLER_VALIDATOR);
+            VALIDATORS.put(GESTURE_ANYWHERE_CHANGED, GESTURE_ANYWHERE_CHANGED_VALIDATOR);
+            VALIDATORS.put(GESTURE_ANYWHERE_ENABLED, GESTURE_ANYWHERE_ENABLED_VALIDATOR);
+            VALIDATORS.put(GESTURE_ANYWHERE_POSITION, GESTURE_ANYWHERE_POSITION_VALIDATOR);
+            VALIDATORS.put(GESTURE_ANYWHERE_SHOW_TRIGGER,
+                           GESTURE_ANYWHERE_SHOW_TRIGGER_VALIDATOR);
+            VALIDATORS.put(GESTURE_ANYWHERE_TRIGGER_HEIGHT,
+                           GESTURE_ANYWHERE_TRIGGER_HEIGHT_VALIDATOR);
+            VALIDATORS.put(GESTURE_ANYWHERE_TRIGGER_TOP,
+                           GESTURE_ANYWHERE_TRIGGER_TOP_VALIDATOR);
+            VALIDATORS.put(GESTURE_ANYWHERE_TRIGGER_WIDTH,
+                           GESTURE_ANYWHERE_TRIGGER_WIDTH_VALIDATOR);
         };
         // endregion
     }
